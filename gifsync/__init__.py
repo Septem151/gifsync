4a5f524460701cab42675321bf9229ecc3adb65e
Carson Mullins <carsonmullins@yahoo.com>
2020-06-11 19:04:06 -0500

    Add favicon.ico

--- gifsync/__init__.py
+++ gifsync/__init__.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home/")
+def home(title='Home'):
+    return render_template('about.html', title=title)
+
+
 @app.route("/about/")
 def about():
-    return render_template('about.html', title='Home')
+    return home('About')
 
 
 @app.route("/collection/")
@@ -24,3 +29,8 @@ def create():
 def show():
     return render_template('show.html', title='Synced Gif')
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
