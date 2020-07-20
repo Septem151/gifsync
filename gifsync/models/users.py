99fcda826bf1226ce3d46fc21fc9c9ff7168a71c
Carson Mullins <carsonmullins@yahoo.com>
2020-07-20 10:08:16 -0500

    Hotfix spotify ads playing
    When spotify is playing ads, item = null in the response. Additionally, check for if user is playing a local track.

--- gifsync/models/users.py
+++ gifsync/models/users.py
@@ -53,11 +53,9 @@ class SpotifyUser(db.Model):
         self.curr_song = {}
         if response.status_code == 200:
             content = response.json()
-            # Don't trust spotify to respect 204 vs 200
-            # When an ad is playing for users w/out premium, content = None, so we must check first
-            if not content:
-                self.curr_song['paused'] = 'true'
-            elif 'item' in content and 'name' in content['item']:
+            # When an ad is playing for users w/out premium, content['item'] = None, so we must check first
+            # Additionally, when user is playing a local file, state that we are paused
+            if content['item'] and not content['item']['is_local']:
                 self.curr_song['name'] = content['item']['name']
                 self.curr_song['id'] = content['item']['id']
                 self.curr_song['tempo'] = Song.get_song_tempo(self.curr_song['id'], self.access_token)
